51f6926583ae7dc413aceb189d6ef6c5834c7ac1
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-06-27 03:47:03 -0400

    Fixed API routes, current weather works correctly now

--- assets/js/weatherApp.js
+++ assets/js/weatherApp.js
@@ -1,4 +1,4 @@
-// Render past cities buttons from local storage:
+// Render search history from local storage:
 let pastCities = localStorage.getItem("city");
 if (pastCities !== null) {
     pastCities = JSON.parse(pastCities);
@@ -10,16 +10,18 @@ if (pastCities !== null) {
 
 // Functions:
 
-// Adding city names to the City History list
+// Adding city names to the search history:
 const addToSearchHistory = () => {
     const cityName = $("#city_search").val().trim();
     $("#search_history").prepend(`<a class="waves-effect waves-light-blue btn-flat">${cityName}</a>
     <br>`);
 }
-// Empty search input field
+
+// Empty search input field:
 const emptySearchInput = () => {
     $("#city_search").val("");
 }
+
 // Storing cities in local storage:
 const storeToLocalStorage = () => {
     const cityName = $("#city_search").val().trim();
@@ -36,105 +38,47 @@ const storeToLocalStorage = () => {
 }
 
 // First ajax call:
-function ajaxCall(cityName) {
-    var APIkey = "031f158b8aa6738886dd6a6cbc74318e";
-    var queryURL1 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIkey;
 
+const ajaxCall = (cityName) => {
+    const APIkey = "031f158b8aa6738886dd6a6cbc74318e";
+    const queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIkey;
+    console.log(cityName);
     $.ajax({
         url: queryURL1,
         method: "GET"
-    }).then(function (response1) {
-        console.log(response1);
+    }).then(function (res1) {
+
         // Collect data and store it into variables:
-        var name = response1.city.name;
-        var description = response1.list[0].weather[0].description;
-        var icon = response1.list[0].weather[0].icon;
-        var temp = response1.list[0].main.temp;
-        var hum = response1.list[0].main.humidity;
-        var wind = response1.list[0].wind.speed;
+        const weather = {
+            city_name: res1.name,
+            des: res1.weather[0].description,
+            icon: res1.weather[0].icon,
+            temp: res1.main.temp,
+            hum: res1.main.humidity,
+            wind: res1.wind.speed
+        }
 
         // Display data on the page:
-        // City name date and icon:
-        $("<div>").addClass("resHeader");
-        $(".resHeader").text(name);
-        // Brief weather description:
-        $("<div>").addClass("description");
-        $(".description").text(description);
-        // Temperature:
-        $("<div>").addClass("resTemp");
-        $(".resTemp").text("Temperature: " + temp + " F");
-        // Humidity:
-        $("<div>").addClass("resHum");
-        $(".resHum").text("Humidity: " + hum + "%");
-        // Wind Speed:
-        $("<div>").addClass("resWind");
-        $(".resWind").text("Wind speed: " + wind + " MPH");
+        $(".city_name").text(weather.city_name);
+        $(".des").text(weather.des);
+        $(".temp").text(`Temperature: ${weather.temp} F`);
+        $(".hum").text(`Humidity: ${weather.hum}%`);
+        $(".wind").text(`Wind speed: ${weather.wind} MPH`);
 
-        // Use the call to display the 5-day forecast:
-        // Day 1
-        var date = response1.list[8].dt_txt;
-        var date1 = date.slice(0, 10);
-        var icon1 = response1.list[8].weather[0].icon;
-        var temp1 = response1.list[8].main.temp;
-        var hum1 = response1.list[8].main.humidity;
-        $("#day1").addClass("card day forecastBox")
-        $("#day1Date").text(date1);
-        $("#day1Temp").text("Temp: " + temp1 + " F");
-        $("#day1Hum").text("Humidity: " + hum1 + "%");
-        // Day 2
-        $("#day2").addClass("card day forecastBox")
-        var date = response1.list[16].dt_txt;
-        var date2 = date.slice(0, 10);
-        var icon2 = response1.list[16].weather[0].icon;
-        var temp2 = response1.list[16].main.temp;
-        var hum2 = response1.list[16].main.humidity;
-        $("#day2Date").text(date2);
-        $("#day2Temp").text("Temp: " + temp2 + " F");
-        $("#day2Hum").text("Humidity: " + hum2 + "%");
-        // Day 3
-        $("#day3").addClass("card day forecastBox")
-        var date = response1.list[24].dt_txt;
-        var date3 = date.slice(0, 10);
-        var icon3 = response1.list[24].weather[0].icon;
-        var temp3 = response1.list[24].main.temp;
-        var hum3 = response1.list[24].main.humidity;
-        $("#day3Date").text(date3);
-        $("#day3Temp").text("Temp: " + temp3 + " F");
-        $("#day3Hum").text("Humidity: " + hum3 + "%");
-        // Day 4
-        $("#day4").addClass("card day forecastBox")
-        var date = response1.list[32].dt_txt;
-        var date4 = date.slice(0, 10);
-        var icon4 = response1.list[32].weather[0].icon;
-        var temp4 = response1.list[32].main.temp;
-        var hum4 = response1.list[32].main.humidity;
-        $("#day4Date").text(date4);
-        $("#day4Temp").text("Temp: " + temp4 + " F");
-        $("#day4Hum").text("Humidity: " + hum4 + "%");
-        // Day 5
-        $("#day5").addClass("card day forecastBox")
-        var date = response1.list[39].dt_txt;
-        var date5 = date.slice(0, 10);
-        var icon5 = response1.list[39].weather[0].icon;
-        var temp5 = response1.list[39].main.temp;
-        var hum5 = response1.list[39].main.humidity;
-        $("#day5Date").text(date5);
-        $("#day5Temp").text("Temp: " + temp5 + " F");
-        $("#day5Hum").text("Humidity: " + hum5 + "%");
-
-        //Second ajax call (by coordinates):
-        var lat = response1.city.coord.lat;
-        var lon = response1.city.coord.lon;
-        var querryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + lat + "&lon=" + lon;
+        //Collect lat and lon and make the second API call:
+        const lat = res1.coord.lat;
+        const lon = res1.coord.lon;
+        const querryURL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly}&appid=${APIkey}`
         $.ajax({
             url: querryURL2,
             method: "GET"
         })
-            // Display  UV data on the page
-            .then(function (response2) {
-                var UV = response2.value;
-                $("<div>").addClass("resUV");
-                $(".resUV").text("UV Index: " + UV);
+            .then(function (res2) {
+                console.log(res2)
+                // Display  UV data on the page
+                const UV = res2.current.uvi;
+                $(".UV").text(`UV: ${UV}`);
+
             })
     })
 }
@@ -146,8 +90,8 @@ searchBtn.on("click", event => {
     event.preventDefault();
     addToSearchHistory();
     storeToLocalStorage();
+    ajaxCall($("#city_search").val());
     emptySearchInput();
-    // ajaxCall($(".form-control").val());
 })
 
 // Reset button:
@@ -158,9 +102,9 @@ resetBtn.on("click", function () {
     localStorage.clear();
 })
 
-// Past city names list:
-var searchHistoryEntries = [$(".listItem")]
-console.log(searchHistoryEntries[0]);
+// Search history buttons:
+// var searchHistoryEntries = [$(".listItem")]
+// console.log(searchHistoryEntries[0]);
 // for (var k = 0; k < historyBtn.length; k++) {
 //     historyBtn[k].on("click", function (event) {
 //         event.preventDefault();
